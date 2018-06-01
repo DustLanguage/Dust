c72e5e41b602bd79de9653f83421a9f16ead26f9
Nufflee <zifcani@gmail.com>
2018-06-01 20:19:04 +0200

    Add range to SyntaxToken.

--- Dust/Compiler/Lexer/SyntaxToken.cs
+++ Dust/Compiler/Lexer/SyntaxToken.cs
@@ -4,6 +4,36 @@
   {
     public SyntaxTokenKind Kind { get; set; }
     public string Text { get; set; }
-    public SourcePosition Position { get; set; }
+
+    public SourcePosition Position
+    {
+      get
+      {
+        if (position == null)
+        {
+          return position = Range.Start;
+        }
+
+        return position;
+      }
+      set => position = value;
+    }
+
+    public SourceRange Range
+    {
+      get
+      {
+        if (range == null)
+        {
+          return range = new SourceRange(position, position);
+        }
+
+        return range;
+      }
+      set => range = value;
+    }
+
+    private SourcePosition position;
+    private SourceRange range;
   }
 }
